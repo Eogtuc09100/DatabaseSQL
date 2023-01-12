5e1afe9ec6ab11875b3cca84b69c7c95a0328085
Eogtuc09100 <eogtuc0910@highgateschool.org.uk>
2023-01-12 10:29:41 +0000

    done

--- read_db.py
+++ read_db.py
@@ -17,4 +17,22 @@ print(first_student)
 print(more_students)
 print(other_students)
 
+average_query = """ 
+SELECT avg(age) 
+FROM students 
+WHERE gender = ? 
+"""
+average_age = cursor.execute(average_query, ('female',)).fetchone()[0]
+
+print(average_age)
+
+group_by_query = """ 
+SELECT gender, avg(age) 
+FROM students 
+GROUP BY gender 
+"""
+average_age_by_gender = cursor.execute(group_by_query).fetchall()
+
+print(average_age_by_gender)
+
 conn.close()
